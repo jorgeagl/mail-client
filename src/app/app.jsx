5534804015af97c4c2eeecbf9d3de9eec102055a
desarrollador.seis <dev@dev.com>
2018-05-10 10:01:41 -0500

    add components inbox and detail

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+
+import Inbox from './components/inbox/inbox';
+import Details from './components/details/details';
+
 import './app.scss';
 
-const App = () => <h1>Hola React
-</h1>
+class Welcome extends React.Component {
+    contructor(prop){
+        //this.state = {name: 'Medium'};
+    }
+    
+}
 
 
-render(
-    <App />,
-    document.getElementById('app')
-)
+ReactDOM.render(<Inbox />, document.getElementById('inbox'));
+ReactDOM.render(<Details />, document.getElementById('details'));
---
+++ src/app/components/details/details.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+//import './styles.scss';
+
+class Details extends React.Component {
+    contructor(){}
+
+    render() {
+        return (
+            <div>Details</div>
+        )
+    }
+}
+
+export default Details;
